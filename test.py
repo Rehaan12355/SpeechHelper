2a258ab4b6a13f1862b3d8aaca9d2243b0127fd8
Rehaan12355 <rehaan_irfan@outlook.com>
2026-09-06 16:49:30 -0700

    Updated to show full face mesh with each landmark point drawn rather than a rectangular box around face

--- test.py
+++ test.py
@@ -1,29 +1,59 @@
 import cv2 as cv
 import numpy as np
+import mediapipe as mp
 
 cap = cv.VideoCapture(0)
-
-face_cascade = cv.CascadeClassifier(
-    cv.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 if not cap.isOpened():
     print("cannot open camera")
     exit()
+    
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
 
-while True:
-    ret, frame = cap.read()
 
-    if not ret:
-        print("Cannot recieve frame, exiting")
-        break
-    gray_image = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    face = face_cascade.detectMultiScale(
-        gray_image, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
-    for (x, y, w, h) in face:
-        cv.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 4)
-    cv.imshow('frame', frame)
-    if cv.waitKey(1) == ord('q'):
-        break
+
+mp_face_mesh = mp.solutions.face_mesh
+
+
+LIP_LANDMARKS = [
+    61, 146, 91, 181, 84, 17, 314, 405, 321, 375, 291,
+    185, 40, 39, 37, 0, 267, 269, 270, 409
+]
+
+
+with mp_face_mesh.FaceMesh(
+    max_num_faces=1,
+    refine_landmarks=True,
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5
+) as face_mesh:
+    while True:
+    
+        ret, frame = cap.read()
+        if not ret:
+            print("Cannot recieve frame, exiting")
+            break            
+        results = face_mesh.process(frame)
+        if results.multi_face_landmarks:
+            for face_landmarks in results.multi_face_landmarks:
+                mp_drawing.draw_landmarks(
+                    image=frame,
+                    landmark_list = face_landmarks,
+                    connections = mp_face_mesh.FACEMESH_TESSELATION,
+                    landmark_drawing_spec = None,
+                    connection_drawing_spec = mp_drawing_styles.get_default_face_mesh_tesselation_style()
+                )
+                mp_drawing.draw_landmarks(
+                    image=frame,
+                    landmark_list = face_landmarks,
+                    connections = mp_face_mesh.FACEMESH_CONTOURS,
+                    landmark_drawing_spec = None,
+                    connection_drawing_spec = mp_drawing_styles.get_default_face_mesh_contours_style()
+                )            
+        cv.imshow('frame', frame)
+        if cv.waitKey(1) == ord('q'):
+            break
 
 cap.release()
 cv.destroyAllWindows()
