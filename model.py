2d4a07d997d4b66bd61265546b58475fe67e232b
Rehaan12355 <rehaan_irfan@outlook.com>
2026-09-09 21:59:55 -0700

    Completed CNN

--- model.py
+++ model.py
@@ -20,8 +20,40 @@ class LipReadingModel(nn.Module):
             kernel_size=2
         )
         
+        self.conv2 = nn.Conv2d(
+            in_channels=16,
+            out_channels=32,
+            kernel_size=3,
+            padding=1
+        )
+        
+        
+        self.lstm = nn.LSTM(
+            input_size=16384,
+            hidden_size=128,
+            batch_first=True
+        )
     def forward(self, x):
+        
+        batch_size, num_frames, channels, height, width = x.shape
+        
+        x = x.reshape(
+            batch_size * num_frames,
+            channels,
+            height,
+            width
+        )
+        
         x = self.conv1(x)
         x = self.relu(x)
         x = self.pool(x)
+        
+        x = self.conv2(x)
+        x = self.relu(x)
+        x = self.pool(x)
+        
+        x = x.flatten(start_dim=1)
+        
+        x.reshape(batch_size, num_frames, -1)
+        
         return x
